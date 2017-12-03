6af9fe0d6e940d6eab4d137b022783efd6e11de5
Deepak Singh Rautela <deepak.singh.rautela@ericsson.com>
2017-12-03 23:46:22 +0530

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+@Component({
+  selector: 'my-app',
+  templateUrl: `app/app.component.html`,
+  styles:[`input.ng-invalid{border-left:5px solid red;}
+           input.ng-valid{border-left:5px solid green;}`]
+})
+export class AppComponent {
+  userForm = new FormGroup({
+    name: new FormControl('Deepak',[Validators.required,Validators.minLength(4),Validators.maxLength(10)]),
+    email:new FormControl(),
+    address:new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      postalcode: new FormControl(null,Validators.pattern('^[1-9][0-9]{5}'))
+    })
+  });
+
+  mySubmitHandler(){
+    console.log(this.userForm.value);
+  }
+}
---
+++ app/employee-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'sub-app',
+  template: '<h1>Sub app</h1>'
+})
+export class SubComponent { 
+
+}
